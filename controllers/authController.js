0f7ccd68c8d6389e569a8dcd0dbb1d7df8a928c2
anand-patwa-nenosystems <146060523+anand-patwa-nenosystems@users.noreply.github.com>
2025-05-19 14:38:39 +0530

    Experi  Code

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,7 +59,12 @@ exports.login = async (req, res) => {
     if (!user.isVerified) {
       return res.status(403).json({ message: 'Account not verified' });
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || 'supersecret');
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '365d' } // 1 year expiration
+    );
+
     res.status(200).json({
       status: 'true',
       message: 'Login successful', token
@@ -122,7 +127,8 @@ exports.updatePassword = async (req, res) => {
 
     res.status(200).json({
       status: 'true',
-      message: 'Password updated successfully.' });
+      message: 'Password updated successfully.'
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
