4816feb6bace45b2d7302e1b1507a716de5a4ca3
anand-patwa-nenosystems <146060523+anand-patwa-nenosystems@users.noreply.github.com>
2025-06-14 11:29:05 +0530

    Update User Schema Modal

--- controllers/authController.js
+++ controllers/authController.js
@@ -175,15 +175,6 @@ exports.updateProfile = async (req, res) => {
   }
 };
 
-// exports.getAllUsers = async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.status(200).json({ success: true, users });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: 'Server Error', error: error.message });
-//   }
-// };
-
 
 exports.tempHello = async (req, res) => {
   res.status(200).json({
--- models/User.js
+++ models/User.js
@@ -1,21 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//   email: { type: String, unique: true, required: true },
-//   password: { type: String, required: true },
-//   fullName: { type: String, required: true },
-//   nickname: { type: String },
-//   dateOfBirth: { type: Date },
-//   phone: { type: String },
-//   gender: { type: String, enum: ['Male', 'Female', 'Other'] },
-//   profileImage: { type: String ,default:""},
-//   membershipTier: { type: String, enum: ['basic', 'premium', 'vip'], required: true },
-//   otp: { type: String },
-//   isVerified: { type: Boolean, default: false },
-//   deviceToken: { type: String },
-// });
-
-// module.exports = mongoose.model('User', userSchema);
 
 const mongoose = require('mongoose');
 
@@ -28,7 +10,7 @@ const userSchema = new mongoose.Schema({
   phone: { type: String },
   gender: { type: String, enum: ['Male', 'Female', 'Other'] },
   profileImage: { type: String, default: "" },
-  membershipTier: { type: String, enum: ['basic', 'premium', 'vip'], required: true },
+  membershipTier: { type: String, enum: ['basic', 'premium', 'vip'], },
   isVerified: { type: Boolean, default: false },
   deviceToken: { type: String },
 });
